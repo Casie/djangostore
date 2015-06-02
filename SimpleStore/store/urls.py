d5d6cf3aa33d6bf269d555bc9f2ea40cd63db9f2
casie <casie.levine@gmail.com>
2015-06-02 16:33:09 -0400

    remove from cart method and url update

--- SimpleStore/store/urls.py
+++ SimpleStore/store/urls.py
@@ -3,4 +3,6 @@ import views
 
 urlpatterns = patterns('',
     url(r'^$', views.catalog, name="catalog"),
+    url(r'^cart/$', views.cart, name="cart"),
+    url(r'^cart/remove/$', views.removefromcart, name="remove"),
 )
